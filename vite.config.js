46469c80c24e1e362d439c9d71bad4990d9fcc50
Rena Hashimi <renhas54143@stud.noroff.no>
2024-11-22 19:49:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-    build: {
-      outDir: 'dist', 
-    },
-  },
-});
+  plugins: [react()],
+})
