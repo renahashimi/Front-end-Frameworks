8c149882f35f0b440c3eaff57361e6f4f7e90a23
Rena Hashimi <renhas54143@stud.noroff.no>
2024-11-22 19:27:20 +0100

    Errors // trying to fix them

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Your Vite config options here
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
 });
