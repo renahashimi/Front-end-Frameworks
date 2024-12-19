517e7ed1c8b8711b6e32c0603b4aae4753db5c36
Rena Hashimi <renhas54143@stud.noroff.no>
2024-12-19 12:11:47 +0100

    Removed image - added react icon

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import cartImage from './../../images/shopping-cart.png'; 
+import { IoCartOutline } from "react-icons/io5"; 
 import CartItemsCount from "../CartItemCount";
 
 function CartIcon() {
     return (
         <a href="/shoppingcart" className="relative flex items-center mx-auto m-2">
-            <img 
-                src={cartImage} 
-                alt="Cart" 
-                className="w-10 h-10"
-            />
+            <IoCartOutline className="w-9 h-9 text-custom-deep" />
             <div className="absolute top-[-5px] right-[-15px] bg-[#EEE5EE] text-black text-sm w-6 h-6 p-1 rounded-full flex justify-center items-center font-medium border-1 border-black">
                 <CartItemsCount />
             </div>
