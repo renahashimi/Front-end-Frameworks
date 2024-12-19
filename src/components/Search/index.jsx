39aa3c75856edcc481b70f9ebf82b7a597f6718d
Rena Hashimi <renhas54143@stud.noroff.no>
2024-12-19 12:10:58 +0100

    Update search function

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
 
 function Search({ searchTerm, setSearchTerm }) {
     const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value); 
+        setSearchTerm(event.target.value);
     };
 
     return (
-        <div className="mb-4 w-full flex justify-center">
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                placeholder="Search for products..."
-                className="px-4 py-2 w-full sm:w-1/2 lg:w-1/3 border rounded"
-            />
+        <div className="my-4 w-full flex justify-center items-center">
+            <div className="relative w-full md:w-1/2">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    placeholder="Search for products..."
+                    className="px-4 py-2 w-full border rounded pl-10" 
+                />
+                <BsSearch
+                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                    size={20}
+                />
+            </div>
         </div>
     );
 }
